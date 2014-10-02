2a85fe5d63cb2229e41b46f40ddc1ca24e8f5592
Andrey Gankov <gav@qsolution.ru>
2014-10-02 11:12:21 +0300

    Добавил Виноградова в авторы, обновил версию

--- CupboardDesigner/Properties/AssemblyInfo.cs
+++ CupboardDesigner/Properties/AssemblyInfo.cs
@@ -1,20 +1,28 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using QSProjectsLib;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle("QS: Конструктор шкафов")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Программа позволяет создавать спецификации шкафов.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("QS: Конструктор шкафов")]
 [assembly: AssemblyCopyright("Quality Solution 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyLogoIcon("CupboardDesigner.icons.logo.png")]
+[assembly: AssemblySupport("", true, "SQLite, Majorsilence Reporting")]
+[assembly: AssemblyAppWebsite("http://www.qsolution.ru/")]
+
+[assembly: AssemblyAuthor("Ганьков Андрей <gav@qsolution.ru>")]
+[assembly: AssemblyAuthor("Виноградов Андрей <vad@qsolution.ru>")]
+
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0.10")]
+[assembly: AssemblyVersion("1.1")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
